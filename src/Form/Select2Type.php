315b4eeaf008c6f1a39bb29c800d76288c469895
sugawara <sugawara@kenyu.co.jp>
2021-08-15 13:52:18 +0900

    Corresponds to the default value

--- src/Form/Select2Type.php
+++ src/Form/Select2Type.php
@@ -4,6 +4,8 @@ namespace Openforce\Select2Bundle\Form;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\ChoiceList\View\ChoiceView;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -31,6 +33,11 @@ class Select2Type extends EntityType
         parent::__construct($registry);
     }
 
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+    }
+
     protected function getFieldKey(array $options)
     {
         $key = $options['class'].self::$field_id_num;
@@ -39,7 +46,7 @@ class Select2Type extends EntityType
         return md5($key);
     }
 
-    public function finishView(FormView $view, FormInterface $form, array $options)
+    public function buildView(FormView $view, FormInterface $form, array $options)
     {
         if($this->request->get("_openforce_select2_request") === $this->getFieldKey($options))
         {
@@ -48,7 +55,56 @@ class Select2Type extends EntityType
 
         $view->vars['related_fields'] = $options['related_fields'];
         $view->vars['field_key'] = $this->getFieldKey($options);
+
+
+        $this->createDefaultValues($options, $view->vars['value'], $view->vars['choices']);
     }
+
+    /**
+     * @param \Doctrine\ORM\EntityManager $em
+     * @param string $class
+     */
+    protected function createDefaultValues($options, $value, &$choices)
+    {
+        foreach((array)$value  as $k => $v)
+        {
+            if(isset($choices[$v]))
+            {
+                if(is_string($value))
+                {
+                    return;
+                }
+                unset($value[$k]);
+            }
+        }
+
+        $em = $options['em'];
+        /** @var \Doctrine\ORM\EntityManager $em */
+        $er = $em->getRepository($options['class']);
+        $meta = $em->getClassMetadata($options['class']);
+        /** @var \Doctrine\ORM\EntityRepository $er */
+
+        $idField = $meta->getIdentifier()[0];
+        $qb = $er->createQueryBuilder("a")
+            ->where("a.{$idField} in(:ids)")
+            ->setParameter("ids", $value);
+        
+        foreach($qb->getQuery()->getResult() as $v)
+        {
+            if($meta->hasField($options['choice_label']))
+            {
+                $label = $meta->getFieldValue($v, $options['choice_label']);
+            }else{
+                $method = $options['choice_label'];
+                $label = $v->$method();
+            }
+
+            $ids = $meta->getIdentifierValues($v);
+            $id = array_shift($ids);
+            $choices[$id] = new ChoiceView($v, $id , $label);
+        }
+    }
+
 
     protected function renderJsonResponse($choices, array $options)
     {
@@ -81,6 +137,7 @@ class Select2Type extends EntityType
             'search_field' => null,
             'max_results' => 50,
             'filter' => function(EntityRepository $er, $word, $relatedFields, $options){
+                
                 $queryBuilder = $er->createQueryBuilder("a");
 
                 if(!$options['search_field'])
@@ -96,6 +153,7 @@ class Select2Type extends EntityType
         ]);
 
         $queryBuilderNormalizer = function (Options $options, $queryBuilder) use($request) {
+            
             $er = $options['em']->getRepository($options['class']);
             $qb = $options['filter']($er, $request->get("term"), $request->get('related_fields'), $options);
             /** @var QueryBuilder $qb */
@@ -105,7 +163,9 @@ class Select2Type extends EntityType
             
             return $qb;
         };
-        $resolver->setNormalizer('query_builder', $queryBuilderNormalizer);     
+        $resolver->setNormalizer('query_builder', $queryBuilderNormalizer);    
+        
+              
 
     }
 
@@ -113,6 +173,5 @@ class Select2Type extends EntityType
     {
         return 'openforce_select2';
     }
-
 
 }
